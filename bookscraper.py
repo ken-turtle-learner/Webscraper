60b20ab49b8db9478a137dc843b615bd0179dba1
irishdawg <tim@timsutton.com>
2022-04-08 14:13:52 -0600

    Built out individual book scraping and csv output

--- bookscraper.py
+++ bookscraper.py
@@ -5,6 +5,7 @@
 
 import requests                   #Imports the requests module
 from bs4 import BeautifulSoup     #Imports the beautiful soup library
+import pandas as pd
 
 ## Create Data Structures
 
@@ -32,18 +33,44 @@ while(status_code != 404):  ## Do this until an error is returned (Status_Code 4
         for books in books:
 
             find_link = books.find("div")
-            book_link = f"http://books.toscrape.com/{find_link.find('a')['href']}"
+            book_link = f"http://books.toscrape.com/catalogue/{find_link.find('a')['href']}"
             if not(book_link in link_queue):
                 link_queue.append(book_link)
 
+    status_code = 404
+
 ## Second, go through each link in the list to populate the book_data dictionary
 
+count = 1
 for link in link_queue:
-    print(link)  # just print for now
-        
-    ##  go through each link 
-    ##  parse the information using Beautiful Soup
-    ##  add to a python dictionary with the title as key and the rest of the data as a dictionary of key/value pairs
+
+    ##  go through each link
+    page = requests.get(link)
+    status_code = page.status_code
+
+    if (status_code == 200):  # 200 means a valid page was returned
+        soup = BeautifulSoup(page.content, "html.parser")
+        book_info = soup.find("div", class_="col-sm-6 product_main")
+        title = book_info.find("h1").text
+
+        ## parse out UPC, Price, Availability
+        table = soup.find('table', class_='table table-striped')
+        rows = table.find_all('tr')   
+
+        UPC = rows[0].find('td').text
+        price = rows[2].find('td').text.replace("£","")
+        availability = rows[5].find('td').text.replace("In stock (", "").replace(" available)","")
+
+        print(f"Book title {count}: {UPC} {title}")
+
+        ##  add to a python dictionary with the title as key and the rest of the data as a dictionary of key/value pairs
+        book_data[UPC] = {'title' : title, 'price' : price, 'availability' : availability}
+
+    count = count + 1
+    
 
 ## Finally, write the library out to an external data store (file or db)
+df = pd.DataFrame(book_data).T
 
+df.to_csv('book_scrapings.csv')
+print("Saved as CSV")
